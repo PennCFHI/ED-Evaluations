cd94b9edb5c4e41d99a6dceb0b97e0593b9a27bb
nadirbilici <nadir@azumio.com>
2015-11-06 14:50:16 -0500

    added QuestionViewController

---
+++ EmergencyEvals/QuestionViewController.h
@@ -0,0 +1,38 @@
+//
+//  QuestionViewController.h
+//  test
+//
+//  Created by Nadir Bilici on 10/29/15.
+//  Copyright © 2015 Nadir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Globals.h"
+
+@interface QuestionViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *competencyName;
+
+@property (weak, nonatomic) IBOutlet UISlider *MilestoneSlider;
+
+@property (weak, nonatomic) IBOutlet UITextView *MilestoneDescription;
+
+@property (strong, nonatomic) NSArray *numbers; 
+
+@property (weak, nonatomic) IBOutlet UIButton *MilestoneNextButton;
+
+@property (nonatomic) int competencyIndex;
+
+@property (nonatomic) int numberMilestonesCompleted;
+
+@property (nonatomic) NSMutableArray *milestoneEvaluations;
+
+@property (weak, nonatomic) IBOutlet UITextField *writtenEvaluation;
+
+@property (weak, nonatomic) IBOutlet UIButton *previousButton;
+
+-(IBAction)changed:(id)sender;
+
+-(int)roundSliderValue:(float)x;
+
+@end
